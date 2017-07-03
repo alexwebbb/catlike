8b07d677e57bde6e3457c64fda6a43bd0a2e6586
Alex Webb <alexwebbb@yahoo.com>
2017-07-03 00:06:34 -0500

    adding rooms

---
+++ Assets/Scripts/MazeDoor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MazeDoor : MazePassage {
+
+    public Transform hinge;
+
+    private MazeDoor OtherSideOfDoor {
+        get {
+            // this will return null when constructing the "front" of the door
+            return otherCell.GetEdge(direction.GetOpposite()) as MazeDoor;
+        }
+    }
+
+    public override void Initialize(MazeCell primary, MazeCell other, MazeDirection direction) {
+        base.Initialize(primary, other, direction);
+        if (OtherSideOfDoor != null) {
+            // flipping the door 
+            // because it is being flipped from the hinge, it is inverted with scale
+            hinge.localScale = new Vector3(-1f, 1f, 1f);
+            Vector3 p = hinge.localPosition;
+            // then the position is adjusted so that it is back at its original position
+            p.x = -p.x;
+            hinge.localPosition = p;
+        }
+    }
+}
