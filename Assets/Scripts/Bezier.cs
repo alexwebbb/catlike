c81fd3303a4f9adab5fd239e9e429f4af1e9d486
Alex Webb <alexwebbb@yahoo.com>
2017-07-15 21:54:06 -0500

    bezier with lerps, I changed it so that bezier is an abstract class

---
+++ Assets/Scripts/Bezier.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public abstract class Bezier : MonoBehaviour {
+
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+        return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+    }
+}
