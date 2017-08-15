29b01a257bdd6f842a473f3beefd08392b204fc9
Alex Webb <alexwebbb@protonmail.com>
2017-08-15 18:26:02 -0500

    just the cube vertex positions

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -18,7 +18,46 @@ public class Cube : MonoBehaviour {
         mesh.name = "Procedural Cube";
         WaitForSeconds wait = new WaitForSeconds(0.05f);
 
-        yield return wait;
+        int cornerVertices = 8;
+        int edgeVertices = (xSize + ySize + zSize - 3) * 4;
+        int faceVertices = (
+            (xSize - 1) * (ySize - 1) +
+            (xSize - 1) * (zSize - 1) +
+            (ySize - 1) * (zSize - 1)) * 2;
+        vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+
+        int v = 0;
+        for (int y = 0; y <= ySize; y++) {
+            for (int x = 0; x <= xSize; x++) {
+                vertices[v++] = new Vector3(x, y, 0);
+                yield return wait;
+            }
+            for (int z = 1; z <= zSize; z++) {
+                vertices[v++] = new Vector3(xSize, y, z);
+                yield return wait;
+            }
+            for (int x = xSize - 1; x >= 0; x--) {
+                vertices[v++] = new Vector3(x, y, zSize);
+                yield return wait;
+            }
+            for (int z = zSize - 1; z > 0; z--) {
+                vertices[v++] = new Vector3(0, y, z);
+                yield return wait;
+            }
+        }
+
+        for (int z = 1; z < zSize; z++) {
+            for (int x = 1; x < xSize; x++) {
+                vertices[v++] = new Vector3(x, ySize, z);
+                yield return wait;
+            }
+        }
+        for (int z = 1; z < zSize; z++) {
+            for (int x = 1; x < xSize; x++) {
+                vertices[v++] = new Vector3(x, 0, z);
+                yield return wait;
+            }
+        }
     }
 
     private void OnDrawGizmos() {
