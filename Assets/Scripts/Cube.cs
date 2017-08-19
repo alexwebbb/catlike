24cdabf147248c62cd79ff21fda89b98ae93329e
Alex Webb <alexwebbb@protonmail.com>
2017-08-19 18:58:59 -0500

    cube generator complete

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -7,7 +7,7 @@ public class Cube : MonoBehaviour {
     public int xSize, ySize, zSize;
 
     private Mesh mesh;
-    private Vector3[] vertices;
+    public Vector3[] vertices;
 
     private void Awake() {
         Generate();
@@ -68,6 +68,8 @@ public class Cube : MonoBehaviour {
         mesh.vertices = vertices;
     }
 
+
+
     private void CreateTriangles() {
         int quads = (xSize * ySize + xSize * zSize + ySize * zSize) * 2;
         int[] triangles = new int[quads * 6];
@@ -81,6 +83,8 @@ public class Cube : MonoBehaviour {
             t = SetQuad(triangles, t, v, v - ring + 1, v + ring, v + 1);
         }
 
+        t = CreateTopFace(triangles, t, ring);
+        t = CreateBottomFace(triangles, t, ring);
         mesh.triangles = triangles;
     }
 
@@ -99,6 +103,72 @@ public class Cube : MonoBehaviour {
 		return index + 6;
 	}
 
+    private int CreateTopFace(int[] triangles, int t, int ring) {
+        int v = ring * ySize;
+        for (int x = 0; x < xSize - 1; x++, v++) {
+            t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + ring);
+        }
+        t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + 2);
+
+        int vMin = ring * (ySize + 1) - 1;
+        int vMid = vMin + 1;
+        // okay.......... I guess this is going to be the first element of the top row
+        int vMax = v + 2;
+
+        for (int z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++) {
+            t = SetQuad(triangles, t, vMin, vMid, vMin - 1, vMid + xSize - 1);
+            for (int x = 1; x < xSize - 1; x++, vMid++) {
+                t = SetQuad(
+                    triangles, t,
+                    vMid, vMid + 1, vMid + xSize - 1, vMid + xSize);
+            }
+            t = SetQuad(triangles, t, vMid, vMax, vMid + xSize - 1, vMax + 1);
+        }
+
+        // okay vmin will be right at the top now... now I see
+        int vTop = vMin - 2;
+        t = SetQuad(triangles, t, vMin, vMid, vMin - 1, vMin - 2);
+        for (int x = 1; x < xSize - 1; x++, vTop--, vMid++) {
+            t = SetQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
+        }
+        t = SetQuad(triangles, t, vMid, vTop - 2, vTop, vTop - 1);
+
+        return t;
+    }
+
+    // who knows man
+    private int CreateBottomFace(int[] triangles, int t, int ring) {
+        int v = 1;
+        int vMid = vertices.Length - (xSize - 1) * (zSize - 1);
+        t = SetQuad(triangles, t, ring - 1, vMid, 0, 1);
+        for (int x = 1; x < xSize - 1; x++, v++, vMid++) {
+            t = SetQuad(triangles, t, vMid, vMid + 1, v, v + 1);
+        }
+        t = SetQuad(triangles, t, vMid, v + 2, v, v + 1);
+
+        int vMin = ring - 2;
+        vMid -= xSize - 2;
+        int vMax = v + 2;
+
+        for (int z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++) {
+            t = SetQuad(triangles, t, vMin, vMid + xSize - 1, vMin + 1, vMid);
+            for (int x = 1; x < xSize - 1; x++, vMid++) {
+                t = SetQuad(
+                    triangles, t,
+                    vMid + xSize - 1, vMid + xSize, vMid, vMid + 1);
+            }
+            t = SetQuad(triangles, t, vMid + xSize - 1, vMax + 1, vMid, vMax);
+        }
+
+        int vTop = vMin - 1;
+        t = SetQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
+        for (int x = 1; x < xSize - 1; x++, vTop--, vMid++) {
+            t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
+        }
+        t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
+
+        return t;
+    }
 
     private void OnDrawGizmos() {
         if (vertices == null) {
