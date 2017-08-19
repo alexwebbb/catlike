45419668629d1e9490742261ba2bbb9876d36c62
Alex Webb <alexwebbb@protonmail.com>
2017-08-19 05:05:25 -0500

    one side quads

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -10,13 +10,19 @@ public class Cube : MonoBehaviour {
     private Vector3[] vertices;
 
     private void Awake() {
-        StartCoroutine(Generate());
+        Generate();
     }
 
-    private IEnumerator Generate() {
+    private void Generate() {
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Cube";
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
+        CreateVertices();
+        CreateTriangles();
+    }
+
+
+    private void CreateVertices() {
+
 
         int cornerVertices = 8;
         int edgeVertices = (xSize + ySize + zSize - 3) * 4;
@@ -30,35 +36,67 @@ public class Cube : MonoBehaviour {
         for (int y = 0; y <= ySize; y++) {
             for (int x = 0; x <= xSize; x++) {
                 vertices[v++] = new Vector3(x, y, 0);
-                yield return wait;
+
             }
             for (int z = 1; z <= zSize; z++) {
                 vertices[v++] = new Vector3(xSize, y, z);
-                yield return wait;
+
             }
             for (int x = xSize - 1; x >= 0; x--) {
                 vertices[v++] = new Vector3(x, y, zSize);
-                yield return wait;
+
             }
             for (int z = zSize - 1; z > 0; z--) {
                 vertices[v++] = new Vector3(0, y, z);
-                yield return wait;
+
             }
         }
 
         for (int z = 1; z < zSize; z++) {
             for (int x = 1; x < xSize; x++) {
                 vertices[v++] = new Vector3(x, ySize, z);
-                yield return wait;
+
             }
         }
         for (int z = 1; z < zSize; z++) {
             for (int x = 1; x < xSize; x++) {
                 vertices[v++] = new Vector3(x, 0, z);
-                yield return wait;
+
             }
         }
+
+        mesh.vertices = vertices;
     }
+
+    private void CreateTriangles() {
+        int quads = (xSize * ySize + xSize * zSize + ySize * zSize) * 2;
+        int[] triangles = new int[quads * 6];
+        int ring = (xSize + zSize) * 2;
+        int t = 0, v = 0;
+
+        // why use q? dont make a damn lick a sense
+        for (int q = 0; q < xSize; q++, v++) {
+            t = SetQuad(triangles, t, v, v + 1, v + ring, v + ring + 1);
+        }
+
+        mesh.triangles = triangles;
+    }
+
+
+    // naming of these is based on image http://catlikecoding.com/unity/tutorials/rounded-cube/03-quad.png
+    private static int SetQuad(int[] triangles, int index, int v00, int v10, int v01, int v11) {
+        // lower left
+		triangles[index] = v00;
+        // upper left
+		triangles[index + 1] = triangles[index + 4] = v01;
+        // lower right
+		triangles[index + 2] = triangles[index + 3] = v10;
+        // upper right
+		triangles[index + 5] = v11;
+        // next index
+		return index + 6;
+	}
+
 
     private void OnDrawGizmos() {
         if (vertices == null) {
